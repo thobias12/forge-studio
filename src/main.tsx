d9215a594667605df30fdcbcd2f96888ae02b0a4
Sletten <thobiasslet@live.dk>
2026-09-14 18:40:34 +0200

    Enable crypt visual detail layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { installPropSelectionAssist } from './lib/propSelectionAssist'
+import { installDungeonVisualAssist } from './lib/dungeonVisualAssist'
 
 installPropSelectionAssist()
+installDungeonVisualAssist()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
