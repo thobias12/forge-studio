a8df38bb15edab246fc43a7b12b135c61bc5e08c
Sletten <thobiasslet@live.dk>
2026-09-17 16:57:27 +0200

    Switch Forge to Animation Studio 3

--- src/pages/AnimationStudioRuntime2.tsx
+++ src/pages/AnimationStudioRuntime2.tsx
@@ -1,14 +1,14 @@
 import AnimationRuntimePanel from '../components/AnimationRuntimePanel'
-import AnimationStudioV2 from './AnimationStudioV2'
+import AnimationStudioV3 from './AnimationStudioV3'
 import '../animation-runtime-2.css'
 
 type Props = {
   onTestGame?: () => void
 }
 
-export default function AnimationStudioRuntime2({ onTestGame }: Props) {
+export default function AnimationStudioRuntime2(_props: Props) {
   return <div className="animation-runtime-2-layout">
-    <AnimationStudioV2 onTestGame={onTestGame}/>
+    <AnimationStudioV3/>
     <AnimationRuntimePanel/>
   </div>
 }
