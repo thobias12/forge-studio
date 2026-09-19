22441b83a96b4c7e084dc27eeb8ca5986ed388f9
Sletten <thobiasslet@live.dk>
2026-09-19 19:43:41 +0200

    Route hidden Equipment Forge QA capture page

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,7 @@ import MapStudio from './pages/MapStudio'
 import PoiForge from './pages/PoiForge'
 import PropForge from './pages/PropForge'
 import Capture from './pages/Capture'
+import EquipmentQaCapture from './pages/EquipmentQaCapture'
 import { installHistoryShortcuts } from './lib/historyShortcuts'
 import { listAssets } from './lib/library'
 import { loadSkillboundWorkspace } from './engine/forgeProject'
@@ -148,6 +149,9 @@ const SKILLBOUND_CONTEXT_PAGES = new Set<Page>(['projects', 'world', 'poiforge',
 
 export default function App() {
   const params = new URLSearchParams(window.location.search)
+  if (params.get('equipmentQa') === '1') {
+    return <EquipmentQaCapture />
+  }
   if (params.get('capture') === '1') return <Capture />
 
   const [page, setPage] = useState<Page>(() => {
